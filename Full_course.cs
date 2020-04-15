8742f04b5929c75616b083a5412a7499a1a0aead
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-15 13:18:22 +0500

    Section 3, Lec 19 (Type conversion)

--- Full_course.cs
+++ Full_course.cs
@@ -6,27 +6,33 @@ namespace HelloWorld
     {
         public static void Main()
         {
-            Console.WriteLine("Hi");
-            byte number=10;
-            int a=2;
-            float price=15.20f;
-            char c='d';
-            string name="mimi";
-            bool isWorking = false;
-            var a1=6;
-            var name2="mimi1";
-            const float pi=3.14;
-        
-            Console.WriteLine(number);
-            Console.WriteLine(price);
-            Console.WriteLine(a);
-            Console.WriteLine(c);
-            Console.WriteLine(name);
-            Console.WriteLine(isWorking);
-            Console.WriteLine(a1);
-            Console.WriteLine(name2);
-            Console.WriteLine("{0} {1}", float.MinValue,float.MaxValue);//string formate.
+            byte a=1;
+            int b=a;
+            Console.WriteLine(b);
+            
+            int c=1;
+            byte d= (byte)c;
+            Console.WriteLine(d);
+            
+            byte num3=255;
+            num3 +=2; 
+            Console.WriteLine(num3); //=1 shows overflow
+            
+            var num="1234";
+            int e=Convert.ToInt32(num);
+            Console.WriteLine(e);
+            
+            //without try catch it overflows and app crashes.
+            try
+            {
+                byte f=Convert.ToByte(num); 
+                Console.WriteLine(f);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No cant be converted");
+            }
+            
         }
-       
     }
 }
