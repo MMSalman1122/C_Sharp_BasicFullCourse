eeacbade9eab0eea9e13bce01f89eca6c323696d
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-16 12:58:00 +0500

    Section 5 Lec 42

--- Full_course.cs
+++ Full_course.cs
@@ -1,1 +1,42 @@
-section 5 Lec 40
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace test
+{
+    public enum Seasons
+    {
+        Spring,
+        Summer,
+        Automun,
+        Winter
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season1 = Seasons.Automun;
+            
+            switch(season1)
+            {
+                case Seasons.Automun:
+                    Console.WriteLine("Its beautiful season.");
+                    break;
+                case Seasons.Summer:
+                    Console.WriteLine("go to beach");
+                    break;
+                default:
+                    Console.WriteLine("No season");
+                    break;
+                
+            }
+            
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
