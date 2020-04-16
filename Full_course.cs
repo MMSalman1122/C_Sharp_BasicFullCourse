ff7b784518259f86e207e6787aa7eb2b8a14788d
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-16 13:19:11 +0500

    Section 5 Lec 48(Random class)

--- Full_course.cs
+++ Full_course.cs
@@ -10,57 +10,12 @@ namespace test
     {
         static void Main(string[] args)
         {
-            for(var i=0; i <= 10;i++)
+            var random = new Random();
+            for(var i=0; i<5;i++)
             {
-                if (i % 2==0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            for (var i=10; i>=0; i--)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            var name = "Salman Ali";
-            for (var i=0 ; i < name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
-            foreach (var charac in name)
-            {
-                Console.WriteLine(charac);
-            }
-            var check = 0;
-
-            while (check<=10)
-            {
-                if (check % 2 == 0)
-                {
-                    Console.WriteLine(check);
-                }
-                check++;
+                Console.WriteLine(random.Next()); 
             }
             
-            while(true)
-            {
-                Console.WriteLine("Enter your name :");
-                var input =Console.ReadLine();
-
-                //if enter
-                if (string.IsNullOrWhiteSpace(input))
-                {
-                    break;
-                }
-                Console.WriteLine("@Echo : " + input);
-            }
-
-            while (true)
-            {
-
-            }
         }
     }
 }
