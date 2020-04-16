e878427ff8cd1ae8d7711ffb4f426a46d235f6ea
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-16 12:59:05 +0500

    Section 5 Lec 47 (Loops)

--- Full_course.cs
+++ Full_course.cs
@@ -5,33 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 namespace test
 {
-    public enum Seasons
-    {
-        Spring,
-        Summer,
-        Automun,
-        Winter
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var season1 = Seasons.Automun;
-            
-            switch(season1)
+            for(var i=0; i <= 10;i++)
             {
-                case Seasons.Automun:
-                    Console.WriteLine("Its beautiful season.");
-                    break;
-                case Seasons.Summer:
-                    Console.WriteLine("go to beach");
-                    break;
-                default:
-                    Console.WriteLine("No season");
-                    break;
-                
+                if (i % 2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            for (var i=10; i>=0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            var name = "Salman Ali";
+            for (var i=0 ; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            foreach (var charac in name)
+            {
+                Console.WriteLine(charac);
+            }
+            var check = 0;
+
+            while (check<=10)
+            {
+                if (check % 2 == 0)
+                {
+                    Console.WriteLine(check);
+                }
+                check++;
             }
             
+            while(true)
+            {
+                Console.WriteLine("Enter your name :");
+                var input =Console.ReadLine();
+
+                //if enter
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                Console.WriteLine("@Echo : " + input);
+            }
 
             while (true)
             {
