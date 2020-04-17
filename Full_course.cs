db77b22497456045270fb2a152258f7812210ac1
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-18 04:02:02 +0500

    Section 6 lec53(Arrays)

--- Full_course.cs
+++ Full_course.cs
@@ -1,1 +1,33 @@
-section 6 lecture 51 and 52 completed
+using System;
+namespace test
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new [] {3,7,9,14,6 };
+
+            Console.WriteLine("Length:" + num.Length);
+            var index=Array.IndexOf(num, 9);
+            Console.WriteLine(index);
+
+            Array.Clear(num, 0, 2);
+            Console.WriteLine("arr[0]:{0},arr[2]:{1}", num[0], num[1]);
+            int [] num2 = new int [3];
+            Array.Copy(num,num2,3);
+            foreach(var a in  num2)
+            {
+                Console.WriteLine(a);
+            }
+            Array.Sort(num);
+            foreach (var a in num)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
