6552c67fcf0b6e1e437baa5ff2e655dd7e5ade57
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-18 04:10:47 +0500

    Section 6 Lec55(Lists)

--- Full_course.cs
+++ Full_course.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 namespace test
 {
 
@@ -6,25 +7,12 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var num = new [] {3,7,9,14,6 };
+            var num = new List<int>() { 1,2,3,4};
+            num.Add(1);
+            num.AddRange(new int[3] { 5,6,7});
 
-            Console.WriteLine("Length:" + num.Length);
-            var index=Array.IndexOf(num, 9);
-            Console.WriteLine(index);
-
-            Array.Clear(num, 0, 2);
-            Console.WriteLine("arr[0]:{0},arr[2]:{1}", num[0], num[1]);
-            int [] num2 = new int [3];
-            Array.Copy(num,num2,3);
-            foreach(var a in  num2)
-            {
+            foreach(var a in num)
                 Console.WriteLine(a);
-            }
-            Array.Sort(num);
-            foreach (var a in num)
-            {
-                Console.WriteLine(a);
-            }
 
             Console.ReadLine();
 
