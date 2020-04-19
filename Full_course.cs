7366b41a0d3221f244333e1b988dec59ad86137a
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-19 08:08:19 +0500

    Section 8 Lec 62,63

--- Full_course.cs
+++ Full_course.cs
@@ -1,18 +1,1 @@
-using System;
-
-namespace test
-{
-
-    class Program
-    {
-        var timeSpan = new TimeSpan(1, 2, 3);
-        var timeSpan1 = new TimeSpan(1, 0, 0);
-        var timeSpan2 = TimeSpan.FromHours(1);
-
-        var start = DateTime.Now;
-        var end = DateTime.Now.AddMinutes(2);
-        var duration = end - start;
-        Console.WriteLine (duration);
-     
-    }
-}
+Section 8 lec 62,63
