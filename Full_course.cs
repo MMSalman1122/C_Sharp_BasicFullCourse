a4e278d35fdfdef1b0279afe0cacef51dff198fb
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-19 07:54:41 +0500

    Section 7 lec 1&2 (Date and time)

--- Full_course.cs
+++ Full_course.cs
@@ -1,1 +1,16 @@
-Section 6 completed
+using System;
+
+namespace test
+{
+
+    class Program
+    {
+        var dateTime = new DateTime(2015, 1, 1);
+        var now = DateTime.Now;
+
+
+        Console.WriteLine("Hour:" + now.Hours);
+        Console.WriteLine("Minute: " + now.Minute); 
+     
+    }
+}
