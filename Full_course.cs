312482ee85b5da38582edb473b46d8ed35aa8988
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-19 08:07:14 +0500

    Section 7 completed

--- Full_course.cs
+++ Full_course.cs
@@ -5,12 +5,14 @@ namespace test
 
     class Program
     {
-        var dateTime = new DateTime(2015, 1, 1);
-        var now = DateTime.Now;
+        var timeSpan = new TimeSpan(1, 2, 3);
+        var timeSpan1 = new TimeSpan(1, 0, 0);
+        var timeSpan2 = TimeSpan.FromHours(1);
 
-
-        Console.WriteLine("Hour:" + now.Hours);
-        Console.WriteLine("Minute: " + now.Minute); 
+        var start = DateTime.Now;
+        var end = DateTime.Now.AddMinutes(2);
+        var duration = end - start;
+        Console.WriteLine (duration);
      
     }
 }
