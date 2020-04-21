f4066036fe66a9314578df96da8267ec8877c3b8
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-21 13:31:14 +0500

    Section 9 Lec 73(File/File I/O)

--- Full_course.cs
+++ Full_course.cs
@@ -1,1 +1,27 @@
-Section 9 Lec 72
+using System.Text;
+
+namespace test
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            var path = @"c:\\temp\\myfile.jpg";
+            File.Copy("c:\\temp\\myfile.jpg","d:\\temp\\myfile.jpg", true); //true is for replace file 
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var contant = File.ReadAllText(path);
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete;
+
+
+            Console.ReadLine();
+        }
+     
+    }
+}
