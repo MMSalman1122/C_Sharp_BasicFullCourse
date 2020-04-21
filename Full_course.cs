27b813dbe11cc96861a7d431d6781557dea3d160
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-21 10:32:40 +0500

    Section 8 practice

--- Full_course.cs
+++ Full_course.cs
@@ -1,1 +1,43 @@
-Section 8 completed
+using System;
+using System.Collections.Generic;
+
+namespace test
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            var sentence = "This is very very very very very very very very very very very very very very very very very very very long line";
+
+            const int maxLength = 20;
+
+            if (sentence.Length<maxLength)
+            {
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                
+                var words = sentence.Split(' ');
+                var totalChar = 0;
+                var summaryword = new List<string>();
+                foreach(var word in words)
+                {
+                    summaryword.Add(word);
+                    totalChar += word.Length + 1;
+                    if(totalChar>maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                var summary = String.Join(" ", summaryword) + "...";
+                Console.WriteLine(summary);
+                Console.ReadLine();
+
+            }
+        }
+     
+    }
+}
